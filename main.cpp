82029897acad125dd0c9491bf226f0ebbd67c0b3
Steven Lu (MBP 2017) <stevenlu443+git-MBP2017@gmail.com>
2020-04-02 18:54:38 +0700

    simplified test case

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "VoxWriter.h"
 
 int main() {
-	vox::VoxWriter vox(120,120,120);
+	vox::VoxWriter vox(127,127,100);
 	vox.AddVoxel(0,0,0,2);
 	vox.AddVoxel(0,1,0,1);
 	vox.AddVoxel(0,0,1,1);
 	vox.AddVoxel(1,0,0,1);
 	vox.AddVoxel(1,1,1,3);
+	
+	for (int i=0; i<127; ++i) {
+		for (int j=0; j<127; ++j) {
+			vox.AddVoxel(i,j, floor(sin((float)(i * i + j * j) / 1000) * 50) + 50,(i+j)%255 + 1);
+		}
+	}
 	vox.SaveToFile("output_voxwriter.vox");
 }
