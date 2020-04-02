69996ccc4b198cf0e9dd29ebe9c17704ff85e20c
Steven Lu (MBP 2017) <stevenlu443+git-MBP2017@gmail.com>
2020-04-02 22:13:31 +0700

    wip

--- main.cpp
+++ main.cpp
@@ -1,17 +1,57 @@
 #include "VoxWriter.h"
 
+// Takes XYZP input file on stdin.
+// XYZP format:
+// Each line has 4 ASCII decimal numbers; they describe a single voxel.
+// The first three are the 0-indexed X, Y, and Z coordinates. The fourth is a 1-255 magicaVoxel
+// palette index.
+// The output dimension is inferred from the input.
+
+#include <iostream>
+#include <string>
+
 int main() {
-	vox::VoxWriter vox(500,500,101);
-	vox.AddVoxel(0,0,0,2);
-	vox.AddVoxel(0,1,0,1);
-	vox.AddVoxel(0,0,1,1);
-	vox.AddVoxel(1,0,0,1);
-	vox.AddVoxel(1,1,1,3);
-	
-	for (int i=0; i<500; ++i) {
-		for (int j=0; j<500; ++j) {
-			vox.AddVoxel(i,j, floor(sin((float)(i * i + j * j) / 10000) * 50) + 50,(i+j)%255 + 1);
+	int lineNr = 0;
+	int minX = INFINITY;
+	int minY = INFINITY;
+	int minZ = INFINITY;
+	int maxX = -INFINITY;
+	int maxY = -INFINITY;
+	int maxZ = -INFINITY;
+	std::vector<unsigned int> data;
+	for (std::string line; std::getline(std::cin, line); ++lineNr) { // parse input
+		std::cout << line << std::endl;
+		int nums[4];
+		std::stringstream ss(line);
+		int ti;
+		int ct = 0;
+		while(ss >> ti) {
+			if (++ct > 4) {
+				printf("invalid line %d \"%s\" has more than 4 numbers in it!\n", lineNr, line.c_str());
+				break;
+			}
+			nums[ct-1] = ti;
 		}
+		if (ct < 4) {
+			printf("invalid line %d \"%s\" has less than 4 numbers in it!\n", lineNr, line.c_str());
+			continue;
+		}
+		if (minX > nums[0]) { minX = nums[0]; }
+		if (minY > nums[1]) { minY = nums[1]; }
+		if (minZ > nums[2]) { minZ = nums[2]; }
+		if (maxX < nums[0]) { maxX = nums[0]; }
+		if (maxY < nums[1]) { maxY = nums[1]; }
+		if (maxZ < nums[2]) { maxZ = nums[2]; }
+		data.push_back(nums[0]);
+		data.push_back(nums[1]);
+		data.push_back(nums[2]);
+		data.push_back(nums[3]);
 	}
+
+	if (minX < 0 || minY < 0 || minZ < 0) {
+		printf("Negative positions found\n");
+	}
+	vox::VoxWriter vox(126,126,126);
+	vox.AddVoxel(i,j, floor(sin((float)(i * i + j * j) / 10000) * 50) + 50,(i+j)%255 + 1);
 	vox.SaveToFile("output_voxwriter.vox");
 }
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
-all: xyzp2vox
+all: xyzp2vox sinTest
 
 xyzp2vox: VoxWriter.cpp main.cpp cTools.cpp
-	g++ -std=c++11 -o $@ $^
+	g++ -O2 -std=c++11 -o $@ $^
+
+sinTest: sinTest.cpp VoxWriter.cpp cTools.cpp
+	g++ -O3 -std=c++11 -o $@ $^
 
 clean:
-	rm xyzp2vox
+	rm xyzp2vox sinTest
